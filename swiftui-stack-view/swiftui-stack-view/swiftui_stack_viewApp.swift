9780229f54481fc2580e2ad4a942319834b9281b
Chandra Welim <chandra.welim@stockbit>
2023-11-11 14:17:28 +0700

    Init project

---
+++ swiftui-stack-view/swiftui-stack-view/swiftui_stack_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_stack_viewApp.swift
+//  swiftui-stack-view
+//
+//  Created by Chandra Welim on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_stack_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
